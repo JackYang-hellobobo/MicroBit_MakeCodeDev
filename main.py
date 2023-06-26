249e16197e2a08e057ba334ea7d5d40057fb50fc
JackYang-hellobobo <68554191+JackYang-hellobobo@users.noreply.github.com>
2023-06-26 13:31:18 +0800

    提交一个MicroBit_V1左右AB键的计数器

--- main.py
+++ main.py
@@ -10,4 +10,33 @@ def ButtonGame():
                 basic.show_string("B")
                 basic.pause(1000)
                 basic.clear_screen()
-ButtonGame()
+
+
+
+def ButtoneCaculate():
+    A = 0
+    B = 0
+    basic.show_number(A)
+    basic.pause(300)
+    basic.show_number(B)
+    basic.pause(300)
+    basic.clear_screen()
+    while True:
+        if input.button_is_pressed(Button.A):
+            if input.button_is_pressed(Button.A):
+                A += 1
+                basic.show_string("A=")
+                basic.show_number(A)
+                basic.pause(20)
+                basic.clear_screen()
+        if input.button_is_pressed(Button.B):
+            if input.button_is_pressed(Button.B):
+                B += 1
+                basic.show_string("B=")
+                basic.show_number(B)
+                basic.pause(20)
+                basic.clear_screen()
+        if on_button_pressed    
+
+
+ButtoneCaculate()
--- main.ts
+++ main.ts
@@ -21,4 +21,38 @@ function ButtonGame() {
     }
 }
 
-ButtonGame()
+function ButtoneCaculate() {
+    let A = 0
+    let B = 0
+    basic.showNumber(A)
+    basic.pause(300)
+    basic.showNumber(B)
+    basic.pause(300)
+    basic.clearScreen()
+    while (true) {
+        if (input.buttonIsPressed(Button.A)) {
+            if (input.buttonIsPressed(Button.A)) {
+                A += 1
+                basic.showString("A=")
+                basic.showNumber(A)
+                basic.pause(20)
+                basic.clearScreen()
+            }
+            
+        }
+        
+        if (input.buttonIsPressed(Button.B)) {
+            if (input.buttonIsPressed(Button.B)) {
+                B += 1
+                basic.showString("B=")
+                basic.showNumber(B)
+                basic.pause(20)
+                basic.clearScreen()
+            }
+            
+        }
+        
+    }
+}
+
+ButtoneCaculate()
